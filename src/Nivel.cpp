0c3fb454a1b0b012b71e179300fac177bc9f953a
rudsomlima <rudsomlima@gmail.com>
2017-07-24 22:52:38 -0300

    Thingspeak

--- src/Nivel.cpp
+++ src/Nivel.cpp
@@ -1,6 +1,6 @@
 #include <LM92.h>
 #include <Wire.h>
-//#include <ThingSpeak.h>
+#include <ThingSpeak.h>
 
 #include <ESP8266WiFi.h>
 //needed for library
@@ -26,10 +26,10 @@ String distance;
 
 WiFiClient  client;
 
-//emoncms
-void sendToEmonCMS(String nodeId, String data1, String data2);
-const char* emoncmsKey = "673d68c21abb07c581469836abc8aa97";
-const char* host = "emoncms.org";
+// //emoncms
+// void sendToEmonCMS(String nodeId, String data1, String data2);
+// const char* emoncmsKey = "673d68c21abb07c581469836abc8aa97";
+// const char* host = "emoncms.org";
 
 //for LED status
 Ticker ticker;
@@ -73,7 +73,7 @@ void setup()
     pinMode(vccPin, OUTPUT);
     digitalWrite(vccPin, HIGH);    //liga a alimentação
 
-    //ThingSpeak.begin(client);
+    ThingSpeak.begin(client);
 }
 
 void loop()
@@ -83,7 +83,7 @@ void loop()
     String s_temp = String(temperatura,1);
     Serial.println(temperatura);
 
-    double_t echoTime = sonar.ping_median(10);  //media de 10 medicoes
+    double_t echoTime = sonar.ping_median(5);  //media de medicoes
     distance = sonar.convert_cm(echoTime);
     Serial.print("Ping: ");
     Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
@@ -91,39 +91,39 @@ void loop()
 
     sendToEmonCMS("0", s_temp, distance);
 
-    //ThingSpeak.writeField(myChannelNumber, 1, temperatura, myWriteAPIKey);
+    ThingSpeak.writeField(myChannelNumber, 1, temperatura, myWriteAPIKey);
     delay(3000);
-    //ESP.deepSleep(sleep_time_s);
+    ESP.deepSleep(sleep_time_s);
 }
 
-void sendToEmonCMS(String nodeId, String data1, String data2) {
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
+  // void sendToEmonCMS(String nodeId, String data1, String data2) {
+  //   const int httpPort = 80;
+  //   if (!client.connect(host, httpPort)) {
+  //     Serial.println("connection failed");
+  //     return;
+  //   }
 
-  // We now create a URI for the request
-  String url = "/input/post.json?node=";
-  url += nodeId;
-  url += "&apikey=";
-  url += emoncmsKey;
-  url += "&csv=";
-  url += data1;
-  url += ",";
-  url += data2;
+  // // We now create a URI for the request
+  // String url = "/input/post.json?node=";
+  // url += nodeId;
+  // url += "&apikey=";
+  // url += emoncmsKey;
+  // url += "&csv=";
+  // url += data1;
+  // url += ",";
+  // url += data2;
 
   //Serial.println(url);
 
   // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  delay(10);
-
-  // Read all the lines of the reply from server
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    //Serial.print(line);
-  }
+  // client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  //              "Host: " + host + "\r\n" +
+  //              "Connection: close\r\n\r\n");
+  // delay(10);
+  //
+  // // Read all the lines of the reply from server
+  // while (client.available()) {
+  //   String line = client.readStringUntil('\r');
+  //   //Serial.print(line);
+  // }
 }
